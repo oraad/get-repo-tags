9eeebdc529001e45752cf06c89dbf24a5ae27bdc
Omar Raad <omarraad@gmail.com>
2022-01-23 12:54:43 +1100

    fix: Update default imports

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,25 @@ require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -15,19 +34,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(2186));
-const github_1 = __importDefault(__nccwpck_require__(5438));
+const core = __importStar(__nccwpck_require__(2186));
+const github = __importStar(__nccwpck_require__(5438));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const githubToken = core_1.default.getInput('github-token');
-            const octokit = github_1.default.getOctokit(githubToken);
-            const [owner, repo] = core_1.default.getInput('repo').split('/');
-            const limit = parseInt(core_1.default.getInput('limit'));
+            const githubToken = core.getInput('github-token');
+            const octokit = github.getOctokit(githubToken);
+            const [owner, repo] = core.getInput('repo').split('/');
+            const limit = parseInt(core.getInput('limit'));
             const tags = [];
             for (let i = 0; tags.length < limit; i++) {
                 const per_page = (limit - tags.length) % 100;
@@ -39,11 +55,11 @@ function run() {
                 });
                 tags.push(...data.map(d => d.name));
             }
-            core_1.default.setOutput('tags', JSON.stringify(tags));
+            core.setOutput('tags', JSON.stringify(tags));
         }
         catch (error) {
             if (error instanceof Error)
-                core_1.default.setFailed(error.message);
+                core.setFailed(error.message);
         }
     });
 }
